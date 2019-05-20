f78bbe1c7963f06b203a1952adf560effd011a9a
Florian Dahlitz <f2dahlitz@freenet.de>
2019-05-20 21:12:24 +0200

    feat(LuckyModels): add serialization and deserialization of models

--- src/main/java/de/glueckscrew/gluecksroulette/models/LuckyCourse.java
+++ src/main/java/de/glueckscrew/gluecksroulette/models/LuckyCourse.java
@@ -1,5 +1,6 @@
 package de.glueckscrew.gluecksroulette.models;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 import lombok.Getter;
@@ -11,12 +12,32 @@ import lombok.Setter;
 public class LuckyCourse {
     private static Logger logger = Logger.getLogger(LuckyCourse.class.getSimpleName());
 
+    private static final String SERIALIZE_DELIMITER = "\n";
+
     @Getter @Setter private String identifier;
     @Getter @Setter private List<LuckyStudent> students;
 
     public LuckyCourse(String identifier, List<LuckyStudent> students) {
         this.identifier = identifier;
         this.students = students;
+    }
+
+    public String serialize() {
+        String serializedCourse = "";
+
+        for(LuckyStudent student : this.getStudents())
+            serializedCourse += student.serialize() + "\n";
+
+        return serializedCourse;
+    }
+
+    public static LuckyCourse deserialize(String data, String identifier) {
+        List<LuckyStudent> students = new ArrayList<>();
+
+        for(String studentData : data.split(SERIALIZE_DELIMITER))
+            students.add(LuckyStudent.deserialize(studentData));
+
+        return new LuckyCourse(identifier, new ArrayList<>(students));
     }
 
     @Override
