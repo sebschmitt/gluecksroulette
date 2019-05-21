4e0b4660e7928a4e0868b5a7e12909228a7289d4
Florian Dahlitz <f2dahlitz@freenet.de>
2019-05-21 14:38:18 +0200

    refactoring(LuckyModels): use uniform formatting

--- src/main/java/de/glueckscrew/gluecksroulette/models/LuckyStudent.java
+++ src/main/java/de/glueckscrew/gluecksroulette/models/LuckyStudent.java
@@ -9,13 +9,19 @@ import lombok.Setter;
 /**
  * @author Florian Dahlitz
  */
-@Data public class LuckyStudent {
+@Data
+public class LuckyStudent {
+
     private static Logger logger = Logger.getLogger(LuckyStudent.class.getSimpleName());
 
     private static final String SERIALIZE_DELIMITER = ",";
 
-    @Getter @Setter private String name;
-    @Getter @Setter private double probability;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private double probability;
 
     private LuckyStudent() {
         this("", 1.0);
@@ -37,8 +43,10 @@ import lombok.Setter;
     public static LuckyStudent deserialize(String data) {
         String[] studentData = data.split(SERIALIZE_DELIMITER);
 
-        if(studentData.length < 1 || studentData.length > 2) {
-            logger.log(Level.SEVERE, String.format("Student data doesn't match the following pattern: name[,probability]\nData: %s", studentData));
+        if (studentData.length < 1 || studentData.length > 2) {
+            logger.log(Level.SEVERE, String.format(
+                "Student data doesn't match the following pattern: name[,probability]%nData: %s",
+                data));
             return null;
         }
 
@@ -46,10 +54,12 @@ import lombok.Setter;
 
         try {
             probability = Double.parseDouble(studentData[1]);
-        } catch(NumberFormatException nfe) {
-            logger.log(Level.SEVERE, "Found an invalid value for probability. Full Trace Back:\n", nfe);
+        } catch (NumberFormatException nfe) {
+            logger.log(Level.SEVERE,
+                String.format("Found an invalid value for probability. Full Trace Back:%n"),
+                nfe);
             return null;
-        } catch(IndexOutOfBoundsException ioobe) {
+        } catch (IndexOutOfBoundsException ioobe) {
             logger.log(Level.FINER, "No value for probability found. Use default value 1.0");
         }
 
