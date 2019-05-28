02f263f9fbb09c3a3358dab31e80c5ef93dc9f9d
paul-weisser <paul-weisser@outlook.com>
2019-05-28 21:51:51 +0200

    added LuckySphere

---
+++ src/main/java/de/glueckscrew/gluecksroulette/playground/LuckySphere.java
@@ -0,0 +1,31 @@
+package de.glueckscrew.gluecksroulette.playground;
+
+import com.sun.javafx.geom.Vec3d;
+import javafx.scene.shape.Sphere;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * This class extends the javafx-sphere-class by velocity.
+ *
+ * @author Paul Weisser
+ */
+
+
+public class LuckySphere extends Sphere {
+    @Getter @Setter
+    Vec3d velocity;
+
+    public LuckySphere (){
+        super();
+        this.velocity = new Vec3d();
+    }
+    public LuckySphere(Vec3d velocity, double radius){
+        super(radius);
+        this.velocity = velocity;
+    }
+    public LuckySphere (Vec3d velocity){
+        super();
+        this.velocity = new Vec3d();
+    }
+}
